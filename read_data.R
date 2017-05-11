102e0b2f500ec253028035c01ef5671c66f45da3
Shaurya Chanana <schanana@wisc.edu>
2017-05-10 21:36:52 -0500

    added ability to handle replicates

--- read_data.R
+++ read_data.R
@@ -11,27 +11,34 @@ get_spectral_file <- function(filename) {
     return(spectral_file)
 }
 
-get_spectral_data <- function(spectral_file) {
-    # Returns only the data without the first column since this column ends up
-    # containing the rownames.
+replicate_remover <- function(spectral_data, replicates) {
+    # Removes the replicates by averaging them into one line (row).
+    temp_matrix = matrix(
+        data = rep(
+            NA,
+            nrow(spectral_data) * ncol(spectral_data) / replicates
+        ),
+        nrow = nrow(spectral_data) / replicates,
+        ncol = ncol(spectral_data)
+    )
+    for (i in 1:(nrow(spectral_data) / replicates)) {
+        temp_matrix[i,] =
+            colMeans(temp_matrix[(replicates * i - replicates + 1):(replicates * i),])
+    }
+    return(as.data.frame(temp_matrix))
+}
+
+get_spectral_data <- function(spectral_file, replicates = 1) {
+    # Returns only the data without the first column since this column has the
+    # rownames.
     spectral_data = as.data.frame(spectral_file[, 2:dim(spectral_file)[2]])
+    if (replicates > 1) {
+        spectral_data = replicate_remover(spectral_data, replicates)
+    }
     return(spectral_data)
 }
 
 # The following functions could be combined. We'll see...
-cleanup_rows <- function(dataframe, dirty_rownames, pattern = "[A-Z]+\\d+") {
-    # Assumes the default naming scheme is <letters><numbers>. It looks for that
-    # combination in the first column of the dataframe produced by the function
-    # 'get_spectral_data'. This must be done
-    library(stringr)
-    dataframe = as.data.frame(dataframe)
-    rownames(dataframe) = str_extract(
-        string = dirty_rownames,
-        pattern = pattern
-        )
-    return(dataframe)
-}
-
 cleanup_cols <- function(dataframe, t = "min") {
     # Makes the column names of the form 'RT_M/Z'.
     library(stringr)
@@ -43,5 +50,28 @@ cleanup_cols <- function(dataframe, t = "min") {
         pattern = pattern,
         replacement = "\\1_\\2"
     )
+    return(dataframe)
+}
+
+
+cleanup_rows <- function(dataframe,
+                         dirty_rownames,
+                         pattern = "[A-Z]+\\d+",
+                         replicates = 1) {
+    # Cleans up rownames. Allows for replicates.
+    library(stringr)
+    dataframe = as.data.frame(dataframe)
+    if (replicates > 1) {
+        repsequence = c(seq(from = 1,
+                            to = length(dirty_rownames),
+                            by = replicates))
+        rownames(dataframe) = str_extract(
+            string = dirty_rownames[repsequence],
+            pattern = pattern)
+    }
+    else {
+    rownames(dataframe) = str_extract(string = dirty_rownames,
+                                      pattern = pattern)
+    }
     return(dataframe)
 }
